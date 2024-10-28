b55b4b9a4b6de417c48fcae3f1edbf7dd75e47a3
zcode-dev <amsharidwan.tkg@gmail.com>
2024-10-28 15:48:20 +0700

    add: Header Sections

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./sections/Header";
+
+const App = () => {
+  return (
+    <main className="overflow-hidden">
+      <Header />
+    </main>
+  )
+};
+
+export default App;
