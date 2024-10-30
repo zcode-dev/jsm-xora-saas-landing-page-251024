8395d3f6d643882be76e49b284c7ab74ee8eb2b5
zcode-dev <amsharidwan.tkg@gmail.com>
2024-10-30 09:52:24 +0700

    add: Hero Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./sections/Header";
+import Hero from "./sections/Hero";
 
 const App = () => {
   return (
     <main className="overflow-hidden">
       <Header />
+      <Hero />
     </main>
-  )
+  );
 };
 
 export default App;
